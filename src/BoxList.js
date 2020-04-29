47b6a3b75e0349d901b95c3e431a36f7f2c62594
tech master <bryan77@twc.com>
2020-04-29 06:41:47 -0400

    create BoxList component to render the boxes

---
+++ src/BoxList.js
@@ -0,0 +1,23 @@
+import React, { Component} from 'react';
+import Box from './Box';
+
+class BoxList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { boxes:[{ height: 25, width: 10, color: "blue" }] }
+    }
+
+    render() {
+        const boxes = this.state.boxes.map(box => (
+            <Box height={box.height} width={box.width} color={box.color} />
+        ));
+        return (
+            <div>
+                <h1>Color Box Maker</h1>
+                { boxes }
+            </div>
+        )
+    }
+}
+
+export default BoxList;
